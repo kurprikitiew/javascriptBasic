7bf6806ae69a892d4a91ee2b0b9b5128b46c15bd
kurprikitiew <kurprikitiew@gmail.com>
2018-11-03 01:45:03 +0700

    continuesLearn;

---
+++ learnjavsript.js
@@ -0,0 +1,43 @@
+	//learnvariable
+
+	"These pretzel are making me thirsty" + "!";
+
+	var george = "These pretzel are making me thirsty" + "!";
+
+	console.log(george);
+
+	var firts= prompt("enter first number");
+	var second= prompt("enter second number");
+	var sum= Number(first) + Number(second);
+	alert("The sum is:" + sum);
+
+	var firstname= prompt("enter firstname");
+	var lastname= prompt("enter lastname");
+	alert("your name is :" + firstname + lastname);
+
+	var a= 34;
+	var b= 21;
+	a= 2;
+	a + b;
+
+	//the answer is 23
+
+	var number1= prompt("firstnumber");
+	var number2= prompt("secondnumber");
+	var sum= Number(firstnumber) + Number(secondnumber);
+	alert(sum);
+
+	var number1= prompt("firstnumber");
+	var number2= prompt("secondnumber");
+	var sum= Number(firstnumber) * Number(secondnumber);
+	alert(sum);
+
+	var number1= prompt("firstnumber");
+	var number2= prompt("secondnumber");
+	var sum= Number(firstnumber) / Number(secondnumber);
+	alert(sum);
+
+	var number1= prompt("firstnumber");
+	var number2= prompt("secondnumber");
+	var sum= Number(firstnumber)- Number(secondnumber);
+	alert(sum);
